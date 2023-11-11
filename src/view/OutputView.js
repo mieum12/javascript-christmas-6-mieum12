3dcfb71c09499ebd819c1438c26c5f84d0a42ee9
mieum12 <qpdlqltb1215@gmail.com>
2023-11-12 02:50:29 +0900

    feat: outputView에 날짜,메뉴,총금액을 출력하는 함수 생성

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,7 +1,51 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
-}
+import { Console } from "@woowacourse/mission-utils";
+import order from "../domain/Order.js";
+import menuItems from "../domain/MenuItems.js";
+
+const OutputView = {
+  /**
+   *
+   * @param {OrderDto} order
+   */
+  printOrderDto(order) {
+    this.printDay(order);
+    this.printAllMenus(order);
+    this.printTotalPrice(order);
+
+    Console.print(`
+<증정 메뉴>
+${order.gift}
+
+<혜택 내역>
+${order.discountDetails}
+
+<총혜택 금액>
+${order.totalDiscount}
+
+<할인 후 예상 결제 금액>
+${order.finalPayment}
+
+<12월 이벤트 배지>
+${order.eventBadge}
+
+`);
+  },
+  printDay(order) {
+    Console.print(
+      `12월 ${order.day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
+    );
+  },
+
+  printAllMenus(order) {
+    Console.print("<주문 메뉴>");
+    const allMenu = order.menuItems;
+    allMenu.forEach((m) => {
+      Console.print(m);
+    });
+  },
+  printTotalPrice(order) {
+    Console.print(`\n<할인 전 총주문 금액>`);
+    Console.print(order.totalPrice);
+  },
+};
+export default OutputView;
