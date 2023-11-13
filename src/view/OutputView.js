c5c5723d3a14832c5f84c87c697716fc10fb49e6
mieum12 <qpdlqltb1215@gmail.com>
2023-11-14 01:31:23 +0900

    fix: 숫자 ,로 구분을 위해 toLocaleString함수 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -29,7 +29,7 @@ const OutputView = {
   },
   printTotalPrice(orderDto) {
     Console.print(`\n<할인 전 총주문 금액>`);
-    Console.print(`${orderDto.priceDetail.totalPrice}원`);
+    Console.print(`${orderDto.priceDetail.totalPrice.toLocaleString()}원`);
   },
   printRewardItem(orderDto) {
     Console.print(`\n<증정 메뉴>`);
@@ -40,11 +40,11 @@ const OutputView = {
   printRewardDetail(orderDto) {
     Console.print(`\n<혜택 내역>`);
     const detailArray = [
-      `크리스마스 디데이 할인: -${orderDto.rewardDetail.dDayDiscountPrice}원`,
-      `평일 할인: -${orderDto.rewardDetail.weekdayDiscountPrice}원`,
-      `주말 할인: -${orderDto.rewardDetail.weekendDiscountPrice}원`,
-      `특별 할인: -${orderDto.rewardDetail.starDayDiscountPrice}원`,
-      `증정 이벤트: -${orderDto.rewardDetail.giftEventPrice}원`,
+      `크리스마스 디데이 할인: -${orderDto.rewardDetail.dDayDiscountPrice.toLocaleString()}원`,
+      `평일 할인: -${orderDto.rewardDetail.weekdayDiscountPrice.toLocaleString()}원`,
+      `주말 할인: -${orderDto.rewardDetail.weekendDiscountPrice.toLocaleString()}원`,
+      `특별 할인: -${orderDto.rewardDetail.starDayDiscountPrice.toLocaleString()}원`,
+      `증정 이벤트: -${orderDto.rewardDetail.giftEventPrice.toLocaleString()}원`,
     ];
     const resultArray = detailArray.filter((d) => !d.includes("-0원"));
     if (resultArray.length > 0) {
@@ -56,14 +56,19 @@ const OutputView = {
 
   printTotalRewardPrice(orderDto) {
     Console.print("\n<총혜택 금액>");
-    Console.print(`${orderDto.rewardDetail.totalRewardPrice}원`);
+
+    Console.print(
+      `${orderDto.rewardDetail.totalRewardPrice.toLocaleString()}원`,
+    );
+    // [에러]
+    // Console.print(`${orderDto.priceDetail.totalRewardPrice}원`);
   },
   printFinalPayment(orderDto) {
     Console.print("\n<할인 후 예상 결제 금액>");
     Console.print(
-      `${
+      `${(
         orderDto.priceDetail.totalPrice - orderDto.rewardDetail.totalRewardPrice
-      }원`,
+      ).toLocaleString()}원`,
     );
   },
   printEventBadge(orderDto) {
