ef1e86ca14b37a63b1e64d8b91a2319c317a5e99
mieum12 <qpdlqltb1215@gmail.com>
2023-11-13 21:01:28 +0900

    feat: 결과를 출력을 위해 전달할 OrderDto 생성

--- src/domain/dto/OrderDto.js
+++ src/domain/dto/OrderDto.js
@@ -1,65 +1,108 @@
 import menuItems from "../MenuItems.js";
+import priceDetail from "../PriceDetail.js";
 
 class OrderDto {
+  /**
+   * @type {number}
+   */
   #day;
+  /**
+   * @type {MenuItemDto[]}
+   */
   #menuItems;
-  #totalPrice;
-  #gift;
-  #discountDetails;
-  #totalDiscount;
-  #finalPayment;
+
+  /**
+   * @type {RewardDetailDto}
+   */
+  #rewardDetail;
+
+  /**
+   * @type {MenuItemDto}
+   */
+  #rewardItem;
+
+  /**
+   * @type {PriceDetailDto}
+   */
+  #priceDetail;
+
+  /**
+   * @type {EventBadge}
+   */
   #eventBadge;
 
+  /**
+   *
+   * @param {number} day
+   * @param {MenuItemDto[]} menuItems
+   * @param {RewardDetailDto} rewardDetail
+   * @param {MenuItemDto} rewardItem
+   * @param {PriceDetailDto} priceDetail
+   * @param {EventBadge} eventBadge
+   */
   constructor(
     day,
     menuItems,
-    totalPrice,
-    gift,
-    discountDetails,
-    totalDiscount,
-    finalPayment,
+    rewardDetail,
+    rewardItem,
+    priceDetail,
     eventBadge,
   ) {
     this.#day = day;
     this.#menuItems = menuItems;
-    this.#totalPrice = totalPrice;
-    this.#gift = gift;
-    this.#discountDetails = discountDetails;
-    this.#totalDiscount = totalDiscount;
-    this.#finalPayment = finalPayment;
+    this.#rewardDetail = rewardDetail;
+    this.#rewardItem = rewardItem;
+    this.#priceDetail = priceDetail;
     this.#eventBadge = eventBadge;
   }
 
+  /**
+   *
+   * @return {number}
+   */
   get day() {
     return this.#day;
   }
 
+  /**
+   *
+   * @return {MenuItemDto[]}
+   */
   get menuItems() {
     return this.#menuItems;
   }
 
-  get totalPrice() {
-    return this.#totalPrice;
+  /**
+   *
+   * @return {RewardDetailDto}
+   */
+  get rewardDetail() {
+    return this.#rewardDetail;
   }
 
-  get gift() {
-    return this.#gift;
+  /**
+   *
+   * @return {MenuItemDto}
+   */
+  get rewardItem() {
+    return this.#rewardItem;
   }
 
-  get discountDetails() {
-    return this.#discountDetails;
+  /**
+   *
+   * @return {PriceDetailDto}
+   */
+  get priceDetail() {
+    return this.#priceDetail;
   }
 
-  get totalDiscount() {
-    return this.#totalDiscount;
-  }
-
-  get finalPayment() {
-    return this.#finalPayment;
-  }
-
+  /**
+   *
+   * @return {EventBadge}
+   */
   get eventBadge() {
     return this.#eventBadge;
   }
 }
+
 export default OrderDto;
