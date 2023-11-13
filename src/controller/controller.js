fbf213571d4bb9a20e60070f036320fcdc16f4c6
mieum12 <qpdlqltb1215@gmail.com>
2023-11-13 15:07:43 +0900

    fix: 순서에 따른 Controller 로직 수정 및 완성

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,13 +3,16 @@ import InputView from "../view/InputView.js";
 
 export class Controller {
   async start() {
-    //주문 생성(날짜 및 메뉴 입력): Order 생성
+    // 1. 주문 생성(날짜 및 메뉴 입력): Order 생성
     const order = await InputView.readOrder();
-    // 할인 혜택 적용
-    // order.discount();
-    // 결과 출력
+    // 2. 할인 혜택 적용
+    order.discount();
+    // 3. 증정 메뉴 주기
+    order.giveRewardItem();
+    // 4. 이벤트 배지 주기
+    order.giveEventBadge();
+    // 5. 결과 출력
     const orderDto = order.makeOrderDto();
-    console.log("✅ orderDto는????", orderDto);
     OutputView.printOrderDto(orderDto);
   }
 }
